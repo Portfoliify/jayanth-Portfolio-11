3accdb72bf83097f96b55475af5855048f84e0f9
Portfoliify <tnmadgaming9920@gmail.com>
2023-12-27 16:47:36 +0530

    first commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -41,15 +41,15 @@ const Landing = () => {
   const about2ImageSource2 = pic10;
   //Content
 
-        const emailAddress = "sample@gmail.com";
-        const navName = "reh gauge 6";
-        const about1FullParagraph = "Hi I am Mohan, As a $photographer I am dedicated to transforming ordinary moments into extraordinary memories.";
-        const about2HeadingFullText = "My Lens, Your $Story";
-        const about2Paragraph = "With a passion for capturing fleeting moments and transforming them into lasting memories, I embark on a visual journey, guided by the light and emotions that surround me. My photography transcends mere images, weaving together narratives that resonate with viewers on a deeper level. Whether it's the raw energy of a live performance, the quiet intimacy of a wedding, or the timeless beauty of a landscape, I strive to create images that reflect the essence of the subject and evoke a sense of connection.";  
-        const about3Paragraph = "If you are looking to discuss a project or just talk photography";
-        const linkedInLink = "https://linkedin.com";
-        const instagramLink = "https://instagram.com";
-        const twitterLink = "https://twitter.com";
+        const emailAddress = "rehman@gmail.com";
+        const navName = "rehh gauge the finals";
+        const about1FullParagraph = "Hi I am Rehmann🗿, As a $developer I am dedicated to transforming ordinary designs into extraordinary uis.";
+        const about2HeadingFullText = "My code, My $UI🗿";
+        const about2Paragraph = "ithula onu ila  kila padi";  
+        const about3Paragraph = "If you are looking to discuss a project or just talk photography pls dont contact me";
+        const linkedInLink = "https://linkedin.com/rehman";
+        const instagramLink = "https://instagram.com/rehman";
+        const twitterLink = "https://twitter.com/rehman";
 //Content
   let about2HeadingHighlight = "",
     about2HeadingTextBefore = about2HeadingFullText,
